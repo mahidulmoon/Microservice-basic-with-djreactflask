fbc20ec578c4656dc72603a8a54acb76db043819
mahidulmoon <mahidulmoon@gmail.com>
2021-03-08 12:04:10 +0600

    setting up api segments

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Product
+from rest_framework.response import Response
+from .serializers import ProductSerializer
 # Create your views here.
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self,request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products,many=True)
+        return Response(serializer.data)
+
+
+
+    def create(self,request):
+        pass
+
+
+    
+    def retrieve(self,request,pk=None):
+        pass
+
+
+    def update(self,request,pk=None):
+        pass
+
+
+    def destroy(self,request,pk=None):
+        pass
