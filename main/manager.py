dc54a605952ba2183281762d82fcdf34be3f5ce2
Mahidul Moon <49182474+mahidulmoon@users.noreply.github.com>
2024-11-19 11:57:00 +0600

    final

--- main/manager.py
+++ main/manager.py
@@ -1,11 +1,11 @@
-from main import app,db
+from main import app, db
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
-migrate = Migrate(app,db)
+migrate = Migrate(app, db)
 
 manager = Manager(app)
-manager.add_command('db',MigrateCommand)
+manager.add_command('db', MigrateCommand)
 
-if __name__ == '_main__':
+if __name__ == '__main__':
     manager.run()
